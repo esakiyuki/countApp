6d9899c5e3d548c802fd8c7609746f35414e19bc
Yuki Esaki <yesaki610@gmail.com>
2020-09-06 04:37:06 +0900

    カウントアプリ

--- count/ViewController.swift
+++ count/ViewController.swift
@@ -14,6 +14,75 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    var number : Int = 0
+    
+    @IBOutlet var label : UILabel!
+    
+    @IBAction func plus() {
+        number = number + 1
+        label.text = String(number)
+        
+        if number >= 10 {
+            label.textColor = UIColor.red
+        } else if number < -9 {
+            label.textColor = UIColor.blue
+        } else {
+            label.textColor = UIColor.black
+        }
+    }
+    
+    @IBAction func minus() {
+        number = number - 1
+        label.text = String(number)
+        
+        if number >= 10 {
+            label.textColor = UIColor.red
+        } else if number < -9 {
+            label.textColor = UIColor.blue
+        } else {
+            label.textColor = UIColor.black
+        }
+    }
+    
+    @IBAction func kakeru() {
+        number = number * 3
+        label.text = String(number)
+        
+        if number >= 10 {
+            label.textColor = UIColor.red
+        } else if number < -9 {
+            label.textColor = UIColor.blue
+        } else {
+            label.textColor = UIColor.black
+        }
+    }
+    
+    @IBAction func waru() {
+        number = number / 5
+        label.text = String(number)
+        
+        if number >= 10 {
+            label.textColor = UIColor.red
+        } else if number < -9 {
+            label.textColor = UIColor.blue
+        } else {
+            label.textColor = UIColor.black
+        }
+    }
+    
+    @IBAction func clear() {
+        number = 0
+        label.text = String(number)
+        
+        if number >= 10 {
+            label.textColor = UIColor.red
+        } else if number < -9 {
+            label.textColor = UIColor.blue
+        } else {
+            label.textColor = UIColor.black
+        }
+    }
 
 
 }
